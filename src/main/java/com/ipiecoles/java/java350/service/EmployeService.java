22efb86320df3ca5f4858f2cc9c711c31e40c8fd
Martin <martin.khapherian@gmail.com>
2021-02-22 15:33:11 +0100

    calculPerformanceCommercial

--- src/main/java/com/ipiecoles/java/java350/service/EmployeService.java
+++ src/main/java/com/ipiecoles/java/java350/service/EmployeService.java
@@ -18,6 +18,7 @@ import java.time.LocalDate;
 public class EmployeService {
 
     private static Logger logger = LoggerFactory.getLogger(EmployeService.class);
+
     @Autowired
     private EmployeRepository employeRepository;
 
@@ -34,40 +35,42 @@ public class EmployeService {
      * @throws EntityExistsException Si le matricule correspond à un employé existant
      */
     public void embaucheEmploye(String nom, String prenom, Poste poste, NiveauEtude niveauEtude, Double tempsPartiel) throws EmployeException, EntityExistsException {
-        logger.info("Embauche d'un employé avec les infos suivantes : nom : {}, prénom : {}, poste {}, niveau d'étude : {}, taux activité : {}",
-                nom, prenom, poste, niveauEtude, tempsPartiel);
+
         //Récupération du type d'employé à partir du poste
         String typeEmploye = poste.name().substring(0,1);
+
         //Récupération du dernier matricule...
         String lastMatricule = employeRepository.findLastMatricule();
         if(lastMatricule == null){
-            logger.warn("Aucun matricule trouvé, matricule initial affecté");
             lastMatricule = Entreprise.MATRICULE_INITIAL;
         }
         //... et incrémentation
         Integer numeroMatricule = Integer.parseInt(lastMatricule) + 1;
         if(numeroMatricule >= 100000){
-            logger.error("Limite des 100000 matricules atteinte !");
             throw new EmployeException("Limite des 100000 matricules atteinte !");
         }
         //On complète le numéro avec des 0 à gauche
         String matricule = "00000" + numeroMatricule;
         matricule = typeEmploye + matricule.substring(matricule.length() - 5);
+
         //On vérifie l'existence d'un employé avec ce matricule
         if(employeRepository.findByMatricule(matricule) != null){
-            logger.error("L'employé de matricule {} existe déjà en BDD", matricule);
             throw new EntityExistsException("L'employé de matricule " + matricule + " existe déjà en BDD");
         }
+
         //Calcul du salaire
         Double salaire = Entreprise.COEFF_SALAIRE_ETUDES.get(niveauEtude) * Entreprise.SALAIRE_BASE;
         if(tempsPartiel != null){
-            salaire = Math.round(salaire * tempsPartiel * 100) / 100d;
+            salaire = salaire * tempsPartiel;
         }
+
         //Création et sauvegarde en BDD de l'employé.
         Employe employe = new Employe(nom, prenom, matricule, LocalDate.now(), salaire, Entreprise.PERFORMANCE_BASE, tempsPartiel);
-        employe = employeRepository.save(employe);
-        logger.info("Employé créé : {}", employe.toString());
+
+        employeRepository.save(employe);
+
     }
+
 
     /**
      * Méthode calculant la performance d'un commercial en fonction de ses objectifs et du chiffre d'affaire traité dans l'année.
